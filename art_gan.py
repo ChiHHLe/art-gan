8f5d0cbc9ebf8c508db2cce6352b523b9ab924c1
ChiHHLe <chihle23@gmail.com>
2020-06-03 22:10:32 -0500

    Art gan

--- art_gan.py
+++ art_gan.py
@@ -11,11 +11,11 @@ import os
 PREVIEW_ROWS = 4
 PREVIEW_COLS = 7
 PREVIEW_MARGIN = 4
-SAVE_FREQ = 100
+SAVE_FREQ = 10
 # Size vector to generate images from
 NOISE_SIZE = 100
 # Configuration
-EPOCHS = 100 #umber of iterations
+EPOCHS = 10 #umber of iterations
 BATCH_SIZE = 32
 GENERATE_RES = 3
 IMAGE_SIZE = 128 # rows/cols
